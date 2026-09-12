1f1ad4320e67d98b474e5aad3d1b234a472fbb50
momentum448-glitch <momentum448@gmail.com>
2026-09-13 00:28:12 +0700

    M2.2: charger enemy (#16)
    * docs: close M2.1 and plan M2.2
    
    * feat: add M2.2 charger enemy
    
    * feat: run M2.2 charger scene
    
    * fix: avoid private method name collision

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import './style.css';
-import { TunedGameScene } from './tuning/TunedGameScene';
+import { ChargerGameScene } from './tuning/ChargerGameScene';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -19,7 +19,7 @@ const config: Phaser.Types.Core.GameConfig = {
       debug: false
     }
   },
-  scene: [TunedGameScene]
+  scene: [ChargerGameScene]
 };
 
 new Phaser.Game(config);
